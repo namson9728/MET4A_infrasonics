5dccf622b768c3ca01c5a6877996a47f8d741a97
Namson Nguyen <nnguyen28@worcester.edu>
2024-01-24 23:13:49 -0500

    Completed reformatting functions in Data_Collection.py

---
+++ Data_Collection.py
@@ -0,0 +1,194 @@
+import pickle
+
+"""
+There are two main data collection formats: standard and non-standard
+
+As the name suggests, standard format is the standard way a data collection
+is store to simplify the data analysis process.
+
+    standard_format = {'specifications': {'filter_number':b'IA=%d'
+                                        'units': {'pressure_units:'bar',
+                                                    'time_units:'sec'
+                                                    }
+                                        },
+                    'data':{'pressures':[],
+                            'times':[]
+                            }
+                    }
+
+Non-standard format refers to the original format by which data was stored
+when it was collected during Summer 2023.
+
+This class contains all the methods to convert from non-standard to standard
+formatas well as other methods to handle data collections in standard format
+to make them compatible with other functions in the program.
+
+Methods
+-------
+load(collection_path:str)
+    Returns the specified pickle file containing a data collection in standard format
+
+load_old(path:str, collection_name:str)
+    Returns the specified pickle file containing a data collection in non-standard format
+
+get_filter(data_dict:dict)
+    Returns the filter setting by which a data collection in standard format was taken
+
+get_filter_old(data_dict:dict)
+    Returns the filter setting by which a data collection in non-standard format was taken
+
+reformat(path:str, collection_name:str,
+         station_names:list=['dol', 'ott', 'sea', 'orc'],
+         pressure_units:str='bar', time_units:str='sec')
+    Returns a reformatted data collection in standard format
+
+save(data_dict:dict, file_path:str)
+    Stores the data collection as a pickle file in the specified path
+"""
+
+def load(collection_path:str):
+    """Retrieves and loads a pickle file of the collection into a dictionary
+    
+    Parameters
+    ----------
+    collection_path : str
+        The full path of the pickle file which contains the collection data of all stations
+
+    Returns
+    -------
+    data_dict : dict
+        A dictionary containing the entire data collection
+    """
+    with open(collection_path, "rb") as f:
+        return pickle.load(f)        
+
+def load_old(path:str, collection_name:str):
+    """Retrieves and loads a specific pickle files of a collection before 2024 into a dictionary
+    
+    Parameters
+    ----------
+    path : str
+        The path to the directory in which the data collection is stored
+
+    collection_name : str
+        The path where the pickle file is stored
+
+    Returns
+    -------
+    data_dict : dict
+        A dictionary containing all the data collection
+    """
+    data_dict = {}
+    sea_mammals = ['dolphin', 'otter', 'seal', 'orca']
+
+    for idx in range(len(sea_mammals)):
+
+        dictionary_name = 'data_dict_' + (sea_mammals[idx])[0:3]
+        pickle_file_name = sea_mammals[idx] + '_' + collection_name
+
+        with open(path + pickle_file_name, "rb") as f:
+            dictionary_name = pickle.load(f)
+        data_dict[idx] = (dictionary_name)
+
+    return data_dict
+
+def get_filter(data_dict:dict):
+    """Retrieves the filter setting of the data collection
+    
+    Parameters
+    ----------
+    data_dict : str
+        The data collection in standard format
+
+    Returns
+    -------
+    filter_number : bytes
+        The filter setting in bytes
+    """
+    filter_number = data_dict['specifications']['filter']
+
+    return filter_number
+
+def get_filter_old(data_dict:dict):
+    """Retrieves the filter setting of the non-reformatted data collection
+    
+    Parameters
+    ----------
+    data_dict : str
+        The data collection
+
+    Returns
+    -------
+    filter_number : bytes
+        The filter setting in bytes
+    """
+    filter_number = str(data_dict[0].keys())
+    filter_number = filter_number.split('=')
+    filter_number = filter_number[1].split('\'')
+    filter_number = int(filter_number[0])
+    filter_number = b'IA=%d' % filter_number
+
+    return filter_number
+
+def reformat(path:str, collection_name:str,
+             station_names:list=['dol', 'ott', 'sea', 'orc'],
+             pressure_units:str='bar', time_units:str='sec'):
+    """Reformats collections before 2024 into new standard collection format
+    
+    Parameters
+    ----------
+    old_collection : dict
+        A dictionary containing collection data after being processed using load_old()
+
+    station_names : list
+        A list of all the station names which will be used as keys for the reformatted dictionary
+        Note: Make sure the order of the station names are in the same order as in old_collection
+        (default ['dol', 'ott', 'sea', 'orc'] - order for collections before 2024)
+
+    pressure_units : str
+        The unit of pressure in which old_collection was taken
+        (default 'bar')
+
+    time_units : str
+        The unit of time in which old_collection was taken
+        (default 'sec')
+
+    Returns
+    -------
+    data_dict : dict
+        The reformatted dictionary containing the entire collection
+    """
+    old_collection = load_old(path, collection_name)
+    filter_num = get_filter_old(old_collection)
+
+    new_dict = {}
+    new_dict['specifications'] = {'filter':filter_num,
+                                    'units':{'pressures':pressure_units,
+                                            'times':time_units}}
+
+    data_dict = {}
+    for idx in range(len(station_names)):
+        p_arr = old_collection[idx][filter_num]['pressures']
+        time_arr = old_collection[idx][filter_num]['times']
+        
+        reformatted_dict = {}
+        reformatted_dict = {'pressures':p_arr, 'times':time_arr}
+
+        data_dict[station_names[idx]] = reformatted_dict
+        new_dict['data'] = data_dict
+
+    return new_dict
+
+def save(data_dict:dict, file_path:str):
+    """Stores the given dataset as a pickle file at the given path
+
+    Parameters
+    ----------
+    data_dict : dict
+        The data collection that will be stored as a pickle file
+    
+    file_path : str
+        The full path at which the pickle file will be saved including the name of the pickle file
+    """
+    with open(file_path, "wb") as f:
+        pickle.dump(data_dict, f)
--- Plot.py
+++ Plot.py
@@ -1,77 +1,103 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Plot:
+"""
+A class that contains all the methods to generate specific plots given the dataset
+
+...
+
+Methods
+-------
+interferometric_response( FIX ME! )
+    Generates a 'interferometric response vs. time' plot for all bandwidths
+
+allan_variance( FIX ME! )
+    Generates a 'phase vs. time' plot of the Allan variance for all bandwidths
+
+correlation( FIX ME! )
+    Generates a 'amplitude vs. frequency' plot of each cross correlation
+
+time_series( FIX ME! )
+    Generates a 'pressure vs. time' plot of the dataset(s)
+
+masterplot( FIX ME! )
+    Generates a comprehensive plot of the timeseries, correlation, Allan variance, and interferometric response
+    
+"""
+
+def interferometric_response():
     """
-    A class that contains all the methods to generate specific plots given the dataset
+    Parameters
+    ----------
 
-    ...
-
-    Methods
+    Returns
     -------
-    interferometric_response( FIX ME! )
-        Generates a 'interferometric response vs. time' plot for all bandwidths
-
-    allan_variance( FIX ME! )
-        Generates a 'phase vs. time' plot of the Allan variance for all bandwidths
-
-    correlation( FIX ME! )
-        Generates a 'amplitude vs. frequency' plot of each cross correlation
-
-    time_series( FIX ME! )
-        Generates a 'pressure vs. time' plot of the dataset(s)
-
-    masterplot( FIX ME! )
-        Generates a comprehensive plot of the timeseries, correlation, Allan variance, and interferometric response
-        
     """
+    return -1
 
-    def interferometric_response():
-        """
-        Parameters
-        ----------
+def allan_variance():
+    """
+    Parameters
+    ----------
 
-        Returns
-        -------
-        """
-        return -1
+    Returns
+    -------
+    """
+    return -1
 
-    def allan_variance():
-        """
-        Parameters
-        ----------
+def correlation():
+    """
+    Parameters
+    ----------
 
-        Returns
-        -------
-        """
-        return -1
+    Returns
+    -------
+    """
+    return -1
 
-    def correlation():
-        """
-        Parameters
-        ----------
+def time_series(data_dict, plot_title:str='Pressure Response'):
+    """Generates a time series plot of the data collection
 
-        Returns
-        -------
-        """
-        return -1
+    Parameters
+    ----------
+    data_dict : dict
+        A properly formatted dictionary containing both 'specifications' and 'data' of the collection
+    plot_title : str
+        The title for the time series plot
+        (default: 'Pressure Response')
+    """
+    plt.figure(figsize=(15, 10))
+    plt.rc('xtick', labelsize=20) 
+    plt.rc('ytick', labelsize=20) 
+    
+    stations = []
+    for key in data_dict['data'].keys():
+        p_arr = data_dict['data'][key]['pressures']
+        time_arr = data_dict['data'][key]['times']
 
-    def time_series():
-        """
-        Parameters
-        ----------
+        stations.append(key)
 
-        Returns
-        -------
-        """
-        return -1
+        plt.plot(time_arr, (np.array(p_arr) - np.mean(p_arr)))
 
-    def master_plot():
-        """
-        Parameters
-        ----------
+    plt.legend(stations,
+        bbox_to_anchor=(1.125, 1.0),
+        loc='upper right', fontsize=20)
 
-        Returns
-        -------
-        """
-        return -1
+    filter_num = str(data_dict['specifications']['filter'])
+    plt.title(plot_title + ' %s' % filter_num, fontsize=40)
+
+    time_unit = str(data_dict['specifications']['units']['times'])
+    plt.xlabel('Time (%s)' % time_unit, fontsize=40)
+
+    pressure_unit = str(data_dict['specifications']['units']['pressures'])
+    plt.ylabel('Pressure (%s)' % pressure_unit, fontsize=40)
+
+def master_plot():
+    """
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+    return -1
